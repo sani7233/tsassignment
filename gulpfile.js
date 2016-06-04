72dd9b727d18872fc3bb53a9eea7b7da6847a770
Sajjad Haider <sani723@gmail.com>
2016-06-04 11:37:28 +0400

    Added header in markup and Sass compile and watch task in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,25 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
 	concat = require('gulp-concat'),
+	sass = require('gulp-sass'),
 	connect = require('gulp-connect');
+
+// Resources path
+var sassSource = ['components/sass/main.scss'];
+
+// Sass task
+gulp.task('styles', function() {
+  gulp.src(sassSource)
+  	  .pipe(sass({outputStyle: 'compressed'}))
+	  .on('error', gutil.log)
+	  .pipe(gulp.dest('builds/development/css'))
+	  .pipe(connect.reload())	  
+});
+
+// Watch task to monitor changes in source
+gulp.task('watch', function() {
+  gulp.watch('components/sass/*.scss', ['styles']); 	
+});
 	
 // Live reload task
 gulp.task("connect", function() {
@@ -12,4 +30,4 @@ gulp.task("connect", function() {
 });
 
 // Default task
-gulp.task('default', ['connect']);
+gulp.task('default', ['styles','connect','watch']);
