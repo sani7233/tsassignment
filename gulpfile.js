85c065d4e9cafcc6482cc079a036a23fc04e7e1f
Sajjad Haider <sani723@gmail.com>
2016-06-04 13:17:01 +0400

    Added navigation and breadcrum styles and gulp task for html monitor

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,13 @@ var gulp = require('gulp'),
 
 // Resources path
 var sassSource = ['components/sass/main.scss'];
+var htmlSource = ['builds/development/*.html'];
+
+// Html task
+gulp.task('html', function() {
+  gulp.src(htmlSource)
+  .pipe(connect.reload());		
+});
 
 // Sass task
 gulp.task('styles', function() {
@@ -13,12 +20,13 @@ gulp.task('styles', function() {
   	  .pipe(sass({outputStyle: 'compressed'}))
 	  .on('error', gutil.log)
 	  .pipe(gulp.dest('builds/development/css'))
-	  .pipe(connect.reload())	  
+	  .pipe(connect.reload());	  
 });
 
 // Watch task to monitor changes in source
 gulp.task('watch', function() {
-  gulp.watch('components/sass/*.scss', ['styles']); 	
+  gulp.watch('components/sass/*.scss', ['styles']); 
+  gulp.watch(htmlSource, ['html']); 	
 });
 	
 // Live reload task
@@ -30,4 +38,4 @@ gulp.task("connect", function() {
 });
 
 // Default task
-gulp.task('default', ['styles','connect','watch']);
+gulp.task('default', ['html','styles','connect','watch']);
