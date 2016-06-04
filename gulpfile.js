da3e0a5ff70ef1446ce0048655e779d1c5bef41e
Sajjad Haider <sani723@gmail.com>
2016-06-04 10:34:25 +0400

    Added Live reloaded functionality in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,15 @@
 var gulp = require('gulp'),
-    gulp-util = require('gulp-util');
+    gutil = require('gulp-util'),
+	concat = require('gulp-concat'),
+	connect = require('gulp-connect');
+	
+// Live reload task
+gulp.task("connect", function() {
+  connect.server({
+    root: 'builds/development/',
+	livereload: true  
+  });
+});
+
+// Default task
+gulp.task('default', ['connect']);
