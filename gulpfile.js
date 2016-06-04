a504ca65c761a33a4abcd391b8a123ea428e907d
Sajjad Haider <sani723@gmail.com>
2016-06-05 01:53:40 +0400

    Added changes to build process for production, not completed

--- gulpfile.js
+++ gulpfile.js
@@ -2,21 +2,42 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
 	concat = require('gulp-concat'),
 	sass = require('gulp-sass'),
+	gulpif = require('gulp-if'),
+	uglify = require('gulp-uglify'),
 	connect = require('gulp-connect');
 
-// Resources path
-var sassSource = ['components/sass/main.scss'];
-var htmlSource = ['builds/development/**/*.html'];
-var jsSource = [
-	'builds/development/app/app.module.js',
-	'builds/development/app/route.module.js',
-	'builds/development/app/login/login.module.js',
-	'builds/development/app/login/login.controller.js',	
-	'builds/development/app/register/register.module.js',
-	'builds/development/app/register/register.modernizr.provider.js',
-	'builds/development/app/register/register.dob.directive.js',
-	'builds/development/app/register/register.password.directive.js',	
-	'builds/development/app/register/register.controller.js',	
+var env,
+	sassSource,
+	htmlSource,
+	jsSource,
+	outputDir,
+	sassStyle;
+	
+	// not orking on windows
+	//env = process.env.NODE_ENV || 'development';
+
+	env = 'development';
+
+	if(env==='development') {
+	  outputDir = 'builds/development/';
+	  sassStyle = 'expanded';
+	} else {
+	  outputDir = 'builds/production/';
+	  sassStyle = 'compressed';	
+	}
+	
+	sassSource = ['components/sass/main.scss'];
+	htmlSource = [outputDir + '**/*.html'];
+	jsSource = [
+	  'builds/development/app/app.module.js',
+	  'builds/development/app/route.module.js',
+	  'builds/development/app/login/login.module.js',
+	  'builds/development/app/login/login.controller.js',	
+	  'builds/development/app/register/register.module.js',
+	  'builds/development/app/register/register.modernizr.provider.js',
+	  'builds/development/app/register/register.dob.directive.js',
+	  'builds/development/app/register/register.password.directive.js',	
+	  'builds/development/app/register/register.controller.js',	
 ];
 
 // Html task
@@ -28,9 +49,9 @@ gulp.task('html', function() {
 // Sass task
 gulp.task('styles', function() {
   gulp.src(sassSource)
-  	  .pipe(sass({outputStyle: 'compressed'}))
+  	  .pipe(sass({outputStyle: sassStyle}))
 	  .on('error', gutil.log)
-	  .pipe(gulp.dest('builds/development/css'))
+	  .pipe(gulp.dest(outputDir + 'css'))
 	  .pipe(connect.reload());	  
 });
 
@@ -38,7 +59,8 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
   gulp.src(jsSource)
   .pipe(concat('script.js'))
-  .pipe(gulp.dest('builds/development/js'))
+  //.pipe(gulpif(env==='production', uglify()))
+  .pipe(gulp.dest(outputDir + 'js'))
   .pipe(connect.reload());
 });
 
@@ -52,7 +74,7 @@ gulp.task('watch', function() {
 // Live reload task
 gulp.task("connect", function() {
   connect.server({
-    root: 'builds/development/',
+    root: outputDir,
 	livereload: true  
   });
 });
